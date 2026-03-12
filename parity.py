36331fdfa004bb4d8a30f5a60b58e96c2ca22e00
Vitalie Ghelbert <64249907+gvitalie@users.noreply.github.com>
2026-03-12 03:24:25 +0200

    Update parity.py

--- parity.py
+++ parity.py
@@ -1,12 +1,11 @@
 def check(n):
-  if not isinstance(n, int):
-    print(f"The value {n} is not an integer value")
-    return
-  if n:
-    if n % 2 == 0:
-      return (n, "even")
-    else:
-      return (n, "odd")
-  else:
-    return (n, "undefined")
-    
+    match n:
+        case _ if not isinstance(n, int):
+            print(f"The value {n} is not an integer value")
+            return
+        case n if n % 2 == 1:
+            return (n, 'odd')
+        case n if n % 2 == 0:
+            return (n, 'even')
+        case 0:
+            return (n, 'undefined')
